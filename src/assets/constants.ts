3e3b3b1ffbe0e27723f1d574b5dff0b1791426d1
esflowers <estrada.flsw@gmail.com>
2025-03-12 23:27:35 -0600

    add name

--- src/assets/constants.ts
+++ src/assets/constants.ts
@@ -1,3 +1,4 @@
 export const title = 'Professional Professor.'
+export const name = 'Estrada Flores, Juan Diego.'
 export const mail = 'estrada.flsw@gmail.com'
 export const linkMail = `mailto:${mail}?subject=Enseñanza.`
