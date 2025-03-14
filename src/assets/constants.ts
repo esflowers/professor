0987527ce9b6d39772f1990bd183ded54e59926a
esflowers <estrada.flsw@gmail.com>
2025-03-13 23:35:55 -0600

    add contants

--- src/assets/constants.ts
+++ src/assets/constants.ts
@@ -1,4 +1,25 @@
 export const title = 'Professional Professor.'
 export const name = 'Estrada Flores, Juan Diego.'
+export const description = 'Docente versado en estudiar.'
 export const mail = 'estrada.flsw@gmail.com'
 export const linkMail = `mailto:${mail}?subject=Enseñanza.`
+export const linkResume = '#'
+export const linkProfessional = 'https://esflowers.icu/'
+export const linkAcademic = 'https://academic.esflowers.icu/'
+
+export const quote = 'Incluso mientras enseñan, los hombres aprenden.'
+export const author = 'Séneca.'
+export const textResume = 'Currículum'
+export const textContact = 'Contactar'
+export const textAbout = 'Sobre mí'
+export const subjectsName = 'Materias'
+export const subjects = ['Ciencias Sociales', 'Ciencias Exactas', 'Programación', 'Tecnología']
+export const menuName = 'Menú'
+export const pages = [
+    //{ name: 'Artículos', link: '/blog' },
+    { name: 'Contacto', link: '/contact' },
+    { name: 'Sobre mí', link: '/about' },
+    //{ name: 'FAQ', link: '/faq' }
+]
+export const page404 = 'Error 404.'
+export const text404 = 'Página no encontrada.'
