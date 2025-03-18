916da1de70ba0b04df8554590caa6e7423eaf149
esflowers <estrada.flsw@gmail.com>
2025-03-17 20:02:03 -0600

    add constants

--- src/assets/constants.ts
+++ src/assets/constants.ts
@@ -2,24 +2,72 @@ export const title = 'Professional Professor.'
 export const name = 'Estrada Flores, Juan Diego.'
 export const description = 'Docente versado en estudiar.'
 export const mail = 'estrada.flsw@gmail.com'
+export const quote = 'Incluso mientras enseñan, los hombres aprenden.'
+export const author = 'Séneca.'
+
 export const linkMail = `mailto:${mail}?subject=Enseñanza.`
 export const linkResume = '#'
 export const linkProfessional = 'https://esflowers.icu/'
 export const linkAcademic = 'https://academic.esflowers.icu/'
 
-export const quote = 'Incluso mientras enseñan, los hombres aprenden.'
-export const author = 'Séneca.'
+export const textAcademic = 'Académico.'
+export const textProgramming = 'Programación.'
 export const textResume = 'Currículum'
 export const textContact = 'Contactar'
 export const textAbout = 'Sobre mí'
+export const textMails = 'Correo electrónico:'
+export const textPortfolios = 'Portafolios:'
+export const text404 = 'Página no encontrada.'
+export const text500 = 'Error interno del servidor.'
+export const textGoBack = 'Regresar'
+export const textFonts = 'Tipografías:'
+export const textDeveloped = 'Desarrollo:'
+export const textAboutSite = 'Sobre este sitio:'
+export const textAboutTeach = 'Mis clases:'
+export const textDesigned = 'Diseñada por'
+export const textDevelopedWith = 'Este sitio fue programado con Astro, TypeScript, Tailwind CSS y HTML; y fue desplegado con Vercel.'
+export const fontSans = 'Epilogue'
+export const fontSerif = 'Gambetta'
+export const fontSansMade = 'Tyler Finck & Etcetera Type Co'
+export const fontSerifMade = 'Indian Type Foundry'
 export const subjectsName = 'Materias'
-export const subjects = ['Ciencias Sociales', 'Ciencias Exactas', 'Programación', 'Tecnología']
 export const menuName = 'Menú'
+
+export const subjects = ['Ciencias Sociales', 'Ciencias Exactas', 'Programación', 'Tecnología']
 export const pages = [
-    //{ name: 'Artículos', link: '/blog' },
     { name: 'Contacto', link: '/contact' },
     { name: 'Sobre mí', link: '/about' },
-    //{ name: 'FAQ', link: '/faq' }
+    { name: 'FAQ', link: '/faq' }
 ]
+
+export const pageContact = 'Contacto.'
+export const pageFaq = 'FAQ.'
 export const page404 = 'Error 404.'
-export const text404 = 'Página no encontrada.'
+export const page500 = 'Error 500.'
+
+export const experiences = [
+    {
+        title: 'Profesor de ciencias.',
+        datetime: '2024 - 2025',
+        datetext: 'Septiembre, 2024 - Enero, 2025',
+        description: 'Impartí clases de Programación, Tecnología y, ocasionalmente, Ciencias Exactas, a estudiantes de entre 8 y 18 años. Diseñé e impartí dos clases semanales enfocadas en aplicaciones, programación y resolución de problemas. Utilicé herramientas de paquetería Office, así como conocimientos prácticos y teóricos de programación para fortalecer el aprendizaje de los estudiantes.',
+        company: 'Centro de Aprendizaje y Desarrollo de Habilidades MindUp'
+    }
+]
+
+export const schools = [
+    {
+        title: 'Profesional Técnico-Bachiller en Informática.',
+        datetime: '2021 - 2024',
+        datetext: 'Agosto, 2021 - Julio, 2024',
+        description: 'Obtuve el título de “Estudiante de Excelencia Académica”, por lo que formé parte del anuario nacional de la escuela. Gané los concursos internos de Razonamiento Matemático, Oratoria y Ajedrez, además, obtuve certificaciones de tecnología, y un reconocimiento por dedicación a la lectura.',
+        company: 'Colegio Nacional de Educación Profesional Técnica'
+    },
+    {
+        title: 'Mayor promedio académico.',
+        datetime: '2015 - 2024',
+        datetext: 'Primaria - Preparatoria',
+        description: 'A lo largo de mi formación escolar, mantuve un desempeño académico destacado, obteniendo el mayor promedio en distintas etapas educativas. Además de destacar en el aula, desarrollé habilidades en razonamiento lógico, comunicación y liderazgo, reflejadas en mi participación en concursos académicos y en mi interés continuo por el aprendizaje.',
+        company: 'Instituciones educativas'
+    }
+]
